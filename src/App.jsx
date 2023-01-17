ad98cf9a34d0a586fc02038a34e059cc9cd56f8b
Aayush Rana <ayushrana38@gmail.com>
2023-01-17 11:12:12 +0530

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <div className="App">
+      <Navbar />
       <h1>Launching Soon 180DSA</h1>
     </div>
   );
